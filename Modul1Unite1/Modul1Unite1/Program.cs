459fc653cde5c822b75bfd50da4bc59b3d11eada
aycansokmen <aycansokmenn@gmail.com>
2025-05-10 23:39:47 +0300

    Modul1Unite1 temizlendi ve .vs klasörü ignore edildi

---
+++ Modul1Unite1/Modul1Unite1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1Unite1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pazartesi");
+            Console.WriteLine("Salı");
+            Console.WriteLine("Çarşamba");
+            Console.WriteLine("Perşembe");
+            Console.WriteLine("Cuma");
+            Console.WriteLine("Cumartesi");
+            Console.WriteLine("Pazar");
+
+            Console.ReadLine();
+        }
+    }
+}
