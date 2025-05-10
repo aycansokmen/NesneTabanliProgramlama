59caffc2037e5ea6bd3ca6a2eb64807abdfa7299
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 00:10:47 +0300

    Modul1Unite3

---
+++ Modul1Unite3/Modul1Unite3/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul1Unite3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int i = 30;
+           // textBox1.Text = i.ToString();
+
+            string i = "Aycan";
+            textBox1.Text = i.ToString();
+
+        }
+    }
+}
