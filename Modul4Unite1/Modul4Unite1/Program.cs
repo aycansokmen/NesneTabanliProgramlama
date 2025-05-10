2ccca9f61bf4e7ae78cd84a784d6155b21ac931a
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 01:27:19 +0300

    Modul4Unite1

---
+++ Modul4Unite1/Modul4Unite1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Modul4Unite1
+{
+    class Bisiklet
+    {
+        int hiz = 0;
+        int vites = 0;
+
+        public Bisiklet()
+        {
+            vites = 18; 
+        }
+
+        public void Hizlan(int artis)
+        {
+            hiz = hiz + artis; 
+        }
+
+        public void BilgileriYaz()
+        {
+            Console.Write("Vites: {0} ", vites);
+            Console.Write("Hız: {0}", hiz);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bisiklet bisiklet1 = new Bisiklet();  
+            Bisiklet bisiklet2;
+            bisiklet2 = bisiklet1;
+            bisiklet1.Hizlan(20); 
+            Console.WriteLine("Bisiklet 1 Bilgileri:");
+            bisiklet1.BilgileriYaz();  
+            Console.WriteLine("\nBisiklet 2 Bilgileri:");
+            bisiklet2.BilgileriYaz();  
+            Console.ReadKey();  
+        }
+    }
+}
