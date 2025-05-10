d12943e02415b632eb13703c3e0bdfd47d7923ac
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 01:30:25 +0300

    Modul4Unite2

---
+++ Modul4Unite2/Modul4Unite2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Modul4Unite2
+{
+    class Program
+    {
+        static void DegerArtir(ref int deger)
+        {
+            deger = deger + 1; 
+        }
+        static void Main(string[] args)
+        {
+            int sayi = 5;
+
+            Console.WriteLine("Başlangıç Değeri: " + sayi);
+            DegerArtir(ref sayi);
+
+            Console.WriteLine("Artırılmış Değer: " + sayi);
+            Console.ReadKey(); 
+        }
+    }
+}
