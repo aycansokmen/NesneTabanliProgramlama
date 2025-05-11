b9804b89346687062a8e5e242f768b2a7e2b2edb
aycansokmen <aycansokmenn@gmail.com>
2025-05-12 00:03:32 +0300

    Modul9Unite1

---
+++ Modul9Unite1/Modul9Unite1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9Unite1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayi = { 1, 2, 3, 4 };
+            int toplam = 0;
+            foreach (int i in sayi)
+            {
+                toplam = toplam + i;
+            }
+            Console.WriteLine("Dizideki sayıların toplamı: " + toplam);
+        }
+    }
+}
