ba7edbc18d14f24fe3345fe9ca585e7705b0b6b8
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 23:33:36 +0300

    Modul8Unite1

---
+++ Modul8Unite1/Modul8Unite1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul8Unite1
+{
+    internal class Program
+    {
+        public delegate void SiralaDelegate(ref string[] kelimeler);
+        static void Main(string[] args)
+        {
+            string[] sozcukler = { "elma", "armut", "muz" };
+            Console.WriteLine("Orijinal Dizi:");
+            DiziYazdir(sozcukler);
+            SiralaDelegate sirala = AZSirala;
+            sirala(ref sozcukler);
+            Console.WriteLine("\nA-Z Sıralı:");
+            DiziYazdir(sozcukler);
+            sirala += ZASirala;
+            Console.WriteLine("\nÇoklu çağrım başlatılıyor (A-Z sonra Z-A):");
+            sirala(ref sozcukler);
+            Console.WriteLine("\nSon Durum:");
+            DiziYazdir(sozcukler);
+            Console.ReadLine();
+        }
+        static void AZSirala(ref string[] kelimeler)
+        {
+            Array.Sort(kelimeler);
+            Console.WriteLine("A-Z sıralandı.");
+        }
+        static void ZASirala(ref string[] kelimeler)
+        {
+            Array.Sort(kelimeler);
+            Array.Reverse(kelimeler);
+            Console.WriteLine("Z-A sıralandı.");
+        }
+        static void DiziYazdir(string[] kelimeler)
+        {
+            foreach (var kelime in kelimeler)
+            {
+                Console.Write(kelime + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
