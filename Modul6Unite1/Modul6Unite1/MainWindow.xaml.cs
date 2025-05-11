55b959e1beadc92ed0f8ef3b2ef3040a9ea01938
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 21:12:45 +0300

    Modul6Unite1

---
+++ Modul6Unite1/Modul6Unite1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Modul6Unite1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, RoutedEventArgs e)
+        {
+            islem();
+        }
+        public void islem()
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int toplam = sayi1 + sayi2;
+            textBox3.Text = toplam.ToString();
+        }
+    }
+}
