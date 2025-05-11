6abc7cc112a413d957c371b3ea50ffae49356a6d
aycansokmen <aycansokmenn@gmail.com>
2025-05-11 21:49:58 +0300

    Modul7Unite2

---
+++ Modul7Unite2/Modul7Unite2/OkulYonetim.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul7Unite2
+{
+    interface IInsan
+    {
+        void Buyu();
+    }
+
+    abstract class OkuyanInsan
+    {
+        public void Oku()
+        {
+            Console.WriteLine("Okudum.");
+        }
+    }
+
+    class Ogrenci : OkuyanInsan, IInsan
+    {
+        public void Buyu()
+        {
+            Console.WriteLine("Hızlı büyüdüm.");
+        }
+    }
+
+    class Ogretmen : OkuyanInsan, IInsan
+    {
+        public void Buyu()
+        {
+            Console.WriteLine("Yavaş büyüdüm.");
+        }
+    }
+}
+
---
+++ Modul7Unite2/Modul7Unite2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul7Unite2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            Ogretmen ogretmen = new Ogretmen();
+
+            ogrenci.Buyu();
+            ogretmen.Buyu();
+
+            ogrenci.Oku();
+            ogretmen.Oku();
+
+            Console.ReadLine();
+        }
+    }
+}
