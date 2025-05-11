dd203bdcdaa8208bf31d7d6dc9a9b40413ea4de4
aycansokmen <aycansokmenn@gmail.com>
2025-05-12 00:08:36 +0300

    Modul9Unite2

---
+++ Modul9Unite2/Modul9Unite2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9Unite2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sayi = new ArrayList();
+            int[] num = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < 10; i++)
+            {
+                sayi.Add(num[i]);
+            }
+            sayi.RemoveAt(1);
+            sayi.Insert(4, 2345);
+            for (int i = 0; i < sayi.Count; i++)
+            {
+                Console.WriteLine(sayi[i]);
+            }
+        }
+    }
+}
